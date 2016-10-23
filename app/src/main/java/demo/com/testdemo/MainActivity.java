ffc619c867abec7de836d282a7d3e73ae2555b3a
Son <thanhsontube@gmail.com>
2016-10-23 07:08:33 +0700

    Add logic test

--- app/src/main/java/demo/com/testdemo/MainActivity.java
+++ app/src/main/java/demo/com/testdemo/MainActivity.java
@@ -1,13 +1,39 @@
 package demo.com.testdemo;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String SENTENCE = "So this is for all of you that have already messed around with Dagger 2";
+
+    /**
+     * Maps size : 15
+     * 1 - So
+     * 2 - this
+     * 3 - is
+     * 4 - for
+     * ---
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private Map<String, Integer> countMap(@NonNull String sentence) {
+        Map<String, Integer> maps = new HashMap<>();
+        return maps;
+    }
+
+    private void print(Map<String, Integer> maps) {
+
     }
 }
